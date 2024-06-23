ae3fdab121a921d7fec45e23974a67571a26cc6e
Vishal Bhardwaj <vishalbhardwaj0571@gmail.com>
2024-06-23 14:33:41 +0530

    removig s from strings

---
+++ TypeCasting.java
@@ -0,0 +1,10 @@
+class TypeCasting{
+
+     public static void main(String[] args)
+    {
+        // Explicit TypeCasting..................
+        float f = 5.6f;
+        int x = (int)f;
+        System.out.println(x);
+    }
+}
